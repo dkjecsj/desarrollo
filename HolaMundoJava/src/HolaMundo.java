60be557b0b5a8243c5b45c77c24859df2fdea7e4
Grisly Paola Beltran Barrios <grislypaolabeltranbarrios@192.168.1.6>
2022-12-02 08:12:04 -0500

    Modificacion clase Hola Mundo Variable

--- HolaMundoJava/src/HolaMundo.java
+++ HolaMundoJava/src/HolaMundo.java
@@ -8,8 +8,8 @@ public class HolaMundo {
     public static void main(String args[]) {
         // Tipo  Nombre  Valor de la variable
         //Definimos la variable
-        int miVariableEnteraPorSiempre = 10;
-        System.out.println(miVariableEnteraPorSiempre);
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
 
         //Cuando la variable ya esta definida , no hay que volver al colocar el tipo de dato.
         //Modificamos el valor de la varible
